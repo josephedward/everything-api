0835f8106fefe5b65e49abae8d37313d94990e58
josephedward <15126922+josephedward@users.noreply.github.com>
2023-05-11 21:54:57 -0400

    openai and dotenv

--- api/index.py
+++ api/index.py
@@ -1,11 +1,42 @@
+import openai
+import os
 from flask import Flask
+
+
+#load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load OpenAI API key from environment variable
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+prompt = """Generate an html document that matches  the following URL path: `{{URL_PATH}}`
+
+add href links to related topics
+
+"""
+
+html_snippet="""
+<!doctype html>
+<html>
+<head><title>Everything Site</title></head>
+<body>
+"""
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/<path:path>')
+def catch_all(path=""):
 
-@app.route('/about')
-def about():
-    return 'About'
+    response = openai.Completion.create(
+      engine="text-davinci-003",
+      prompt=prompt.replace("{{URL_PATH}}",path),
+      max_tokens=1024,
+      n=1,
+      stop=None,
+      temperature=0.7,
+    )
+
+    return html_snippet+response
+    # f"<p>{path}</p>"
