2024fde8de1ed0f308c6aa17a67d181fcc66a5d6
josephedward <15126922+josephedward@users.noreply.github.com>
2023-11-26 00:39:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 import os
 from flask import Flask
 
@@ -7,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Load OpenAI API key from environment variable
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 prompt = """Generate an html document that matches  the following URL path: `{{URL_PATH}}`
 add href links to related topics
@@ -24,12 +26,10 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<path:path>')
 def catch_all(path=""):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt.replace("{{URL_PATH}}", path),
-        max_tokens=512,
-        n=1,
-        temperature=0.7,
-    )
+    response = client.completions.create(engine="text-davinci-003",
+    prompt=prompt.replace("{{URL_PATH}}", path),
+    max_tokens=512,
+    n=1,
+    temperature=0.7)
 
     return html_snippet+response.choices[0].text
